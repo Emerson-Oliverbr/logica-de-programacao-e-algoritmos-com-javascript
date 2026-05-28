1c0b49221657c45d8c46e884563006b1eb8f75df
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-27 23:27:33 -0300

    Principais tipos de dados

---
+++ Modulo 1 Introdução ao Javascript/tipos-de-dados/index.js
@@ -0,0 +1,28 @@
+let numero = 10;
+let numero2 = 10;
+let numero3 = 10.5;
+
+console.log(typeof numero);
+console.log(typeof numero2);
+console.log(typeof numero3);
+
+let nome = "Emerson";
+console.log(typeof(nome));
+
+let indefinida = undefined;
+console.log(typeof indefinida);
+
+let naoDefinida = undefined;
+console.log(typeof naoDefinida);
+
+let testeNulo = null;
+console.log(typeof testeNulo);
+
+let objeto = {};
+console.log(typeof objeto);
+
+let funcao = () => {};
+console.log(typeof funcao);
+
+
+
