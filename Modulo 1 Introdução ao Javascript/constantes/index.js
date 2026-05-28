b2b8d772c1d0c8b2b9700a5988c1a3b9a400397f
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-27 23:33:31 -0300

    Constantes

---
+++ Modulo 1 Introdução ao Javascript/constantes/index.js
@@ -0,0 +1,6 @@
+const nome = "Emerson Oliveira"; // Isso é uma contate valor da "variavel' não muda
+console.log(nome);
+
+/* nome = "Oliveira"; // Erro ao tentar reatribuiar valor a uma constante 
+
+console.log(nome); */
