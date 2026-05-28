a7fa567e2ad88eb8e32b30cf07111d21b9b4e957
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-27 23:52:11 -0300

    Operadores matematicos

---
+++ Modulo 1 Introdução ao Javascript/operadores-matematicos/index.js
@@ -0,0 +1,19 @@
+let numero1 = 10;
+let numero2 = 10;
+
+let resultado = numero1 + numero2;
+console.log(resultado);
+console.log("+---------------------------------------------+");
+console.log(`O resultado da soma de ${numero1} + ${numero2} = ${numero1 + numero2}`);
+console.log(`O resultado da subtracao de ${numero1} - ${numero2} = ${numero1 - numero2}`);
+console.log(`O resultado da multiplicacao de ${numero1} * ${numero2} = ${numero1 * numero2}`);
+console.log(`O resultado da divisao de ${numero1} / ${numero2} = ${numero1 / numero2}`);
+console.log(`O resto da divisao de ${numero1} % ${numero2} = ${numero1 % numero2}`);
+console.log("+---------------------------------------------+");
+
+let numero3 = 33.99;
+let numeroQuebrado = numero3 * numero3;
+
+console.log(numeroQuebrado);
+console.log(numeroQuebrado.toFixed(2));
+console.log(numeroQuebrado.toFixed(3));
