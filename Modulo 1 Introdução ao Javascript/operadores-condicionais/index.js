950603a1ac4e3621c33cf73b429ec07fff153eb0
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-28 21:01:42 -0300

    Operadores condicionais

---
+++ Modulo 1 Introdução ao Javascript/operadores-condicionais/index.js
@@ -0,0 +1,14 @@
+let num1 = 18;
+let num2 = 99;
+let num3 = "99";
+
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num2 === num3);
+console.log(num2 == num3);
+
