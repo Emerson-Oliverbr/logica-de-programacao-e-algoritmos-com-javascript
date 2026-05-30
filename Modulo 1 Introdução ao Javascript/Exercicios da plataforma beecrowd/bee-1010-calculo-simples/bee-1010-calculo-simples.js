2b91c44afda013f142467e446f48dfd42bde2aac
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-30 20:32:38 -0300

    add ultimo exercicio

--- Modulo 1 Introdução ao Javascript/Exercicios da plataforma beecrowd/bee-1010-calculo-simples/bee-1010-calculo-simples.js
+++ Modulo 1 Introdução ao Javascript/Exercicios da plataforma beecrowd/bee-1010-calculo-simples/bee-1010-calculo-simples.js
@@ -1,2 +1,15 @@
 const prompt = require("prompt-sync")();
 
+const linha1 = prompt().split(' ');
+const idPeca1 = parseInt(linha1[0], 10);
+const qtePeca1 = parseInt(linha1[1], 10);
+const valorPeca1 = parseFloat(linha1[2]);
+
+const linha2 = prompt().split(' ');
+const idPeca2 = parseInt(linha2[0], 10);
+const qtePeca2 = parseInt(linha2[1], 10);
+const valorPeca2 = parseFloat(linha2[2]);
+
+const total = (qtePeca1 * valorPeca1) + (qtePeca2 * valorPeca2);
+
+console.log(`VALOR A PAGAR: R$ ${total.toFixed(2)}`);
