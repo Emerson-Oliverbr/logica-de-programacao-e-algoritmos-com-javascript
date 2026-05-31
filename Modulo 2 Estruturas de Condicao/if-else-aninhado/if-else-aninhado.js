32e47ebdbd3530d44a6cd92c0e9c8ba011d90c0f
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-05-30 22:33:05 -0300

    Iniciando estruturas de condição

---
+++ Modulo 2 Estruturas de Condicao/if-else-aninhado/if-else-aninhado.js
@@ -0,0 +1,15 @@
+const nota = 60;
+let mensagem = null;
+
+if(nota >= 80) {
+    mensagem = "Parabéns voce foi aprovado!";
+}
+else if(nota < 80 && nota >= 60) {
+    mensagem = "Voce está na lista de espera!";    
+}
+else {
+    mensagem = "Voce foi reprovado, estude mais!";
+}
+
+console.log(mensagem);
+
---
+++ Modulo 2 Estruturas de Condicao/if-else/index.js
@@ -0,0 +1,10 @@
+//é maior ou menor de idade?
+
+const idade = 19;
+
+if(idade >= 18) {
+    console.log("Maior de idade");
+}
+else {
+    console.log("Menor de idade");
+}
